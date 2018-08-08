b7322a1be6c35a37a465022d99070d5676ae18fa
pmammino <paul.mammino@wagner.edu>
2018-08-07 22:00:10 -0400

    Pull hitting and pitching data

--- MLB Simulator.py
+++ MLB Simulator.py
@@ -3,6 +3,18 @@ import requests
 import pandas
 import timeit
 
+
+
+## Function Definitions
+
+def set_lineups(n):
+  away_lineup = list_lineups[n].reset_index(drop = True)
+  home_lineup = list_lineups[n + 1].reset_index(drop = True)
+  away_pitcher = pandas.DataFrame(pitchers.iloc[n]).T.reset_index(drop = True)
+  home_pitcher = pandas.DataFrame(pitchers.iloc[n+1]).T.reset_index(drop = True)
+  return(away_lineup, home_lineup, away_pitcher, home_pitcher)
+
+
 start = timeit.default_timer()
 
 #
@@ -86,65 +98,66 @@ pitchers["Throws"] = pitcher_handedness
 
 merge = batter_steamer[["name","mlbamid"]].drop_duplicates()
 
-lineups_merged = lineups.merge(merge, on = "name", how = "left")
+lineups_merged = lineups.merge(merge, on = ["name"], how = "left")
+lineups_merged["vRCode"] = "0-vR-" + lineups_merged['mlbamid'].astype(str).replace('\.0', '', regex=True)
+lineups_merged["vLCode"] = "0-vL-" + lineups_merged['mlbamid'].astype(str).replace('\.0', '', regex=True)
 
-test = lineups_merged[lineups_merged['mlbamid'].isnull()]
+lineups_merged = lineups_merged.merge(batter_steamer[["m_id", "BB", "K", "Single", "Double", "Triple", "HR", "BO"]], left_on = ["vRCode"], right_on = ["m_id"], how = "left")
+lineups_merged = lineups_merged.rename(index=str, columns={"BB" : "bbR", "K" : "kR", "Single" : "1bR", "Double" : "2bR", "Triple" : "3bR", "HR" : "hrR", "BO" : "boR"})
 
-#
+lineups_merged = lineups_merged.merge(batter_steamer[["m_id", "BB", "K", "Single", "Double", "Triple", "HR", "BO"]], left_on = ["vLCode"], right_on = ["m_id"], how = "left")
+lineups_merged = lineups_merged.rename(index=str, columns={"BB" : "bbL", "K" : "kL", "Single" : "1bL", "Double" : "2bL", "Triple" : "3bL", "HR" : "hrL", "BO" : "boL"})
+
+
+merge_2 = pitcher_steamer[["name","mlbamid"]].drop_duplicates()
+
+pitchers = pitchers.merge(merge_2, on = ["name"], how = "left")
+pitchers["vRCode"] = "SP-0-vR-" + pitchers['mlbamid'].astype(str).replace('\.0', '', regex=True)
+pitchers["vLCode"] = "SP-0-vL-" + pitchers['mlbamid'].astype(str).replace('\.0', '', regex=True)
+
+pitchers = pitchers.merge(pitcher_steamer[["m_id", "BB", "K", "Single", "Double", "Triple", "HR", "BO"]], left_on = ["vRCode"], right_on = ["m_id"], how = "left")
+pitchers = pitchers.rename(index=str, columns={"BB" : "bbR", "K" : "kR", "Single" : "1bR", "Double" : "2bR", "Triple" : "3bR", "HR" : "hrR", "BO" : "boR"})
+pitchers = pitchers.merge(pitcher_steamer[["m_id", "BB", "K", "Single", "Double", "Triple", "HR", "BO"]], left_on = ["vLCode"], right_on = ["m_id"], how = "left")
+pitchers = pitchers.rename(index=str, columns={"BB" : "bbL", "K" : "kL", "Single" : "1bL", "Double" : "2bL", "Triple" : "3bL", "HR" : "hrL", "BO" : "boL"})
+
+
+n = 9  #chunk row size
+list_lineups = [lineups_merged[i:i+n] for i in range(0,lineups_merged.shape[0],n)]
+
 #
 # manipulate lineup scrape into each specific team's lineup
 
-if lineups.shape[0] > 10:
-    away_lineup1 = lineups[:9]
-    away_pitcher1 = pitchers[:1]
-    home_lineup1 = lineups[10:19]
-    home_pitcher1 = pitchers[1:2]
+  
+away_lineup, home_lineup, away_pitcher, home_pitcher = set_lineups(0)  
 
-print(away_pitcher1)
-print(away_lineup1)
-print(home_pitcher1)
-print(home_lineup1)
 
-away_batter = pandas.DataFrame(away_lineup1.iloc[0])
+def at_bat(pitcher, hitter):
+    
+    if pitcher["Handedness"] == "R"  
+    odds1b = ((batter_p1b / (1 - batter_p1b)) * (pitcher_p1b / (1 - pitcher_p1b)) / (league_p1b / (1 - league_p1b)))
+    odds2b = ((batter_p2b / (1 - batter_p2b)) * (pitcher_p2b / (1 - pitcher_p2b)) / (league_p2b / (1 - league_p2b)))
+    odds3b = ((batter_p3b / (1 - batter_p3b)) * (pitcher_p3b / (1 - pitcher_p3b)) / (league_p3b / (1 - league_p3b)))
+    oddshr = ((batter_phr / (1 - batter_phr)) * (pitcher_phr / (1 - pitcher_phr)) / (league_phr / (1 - league_phr)))
+    oddsbb = ((batter_pbb / (1 - batter_pbb)) * (pitcher_pbb / (1 - pitcher_pbb)) / (league_pbb / (1 - league_pbb)))
+    oddsso = ((batter_pso / (1 - batter_pso)) * (pitcher_pso / (1 - pitcher_pso)) / (league_pso / (1 - league_pso)))
+    oddsbo = ((batter_pbo / (1 - batter_pbo)) * (pitcher_pbo / (1 - pitcher_pbo)) / (league_pbo / (1 - league_pbo)))
 
-#
-#
-# basic odds ratio calculator
-# =============================================================================
-# 
-# odds1b = ((batter_p1b / (1 - batter_p1b)) * (pitcher_p1b / (1 - pitcher_p1b)) / (league_p1b / (1 - league_p1b)))
-# odds2b = ((batter_p2b / (1 - batter_p2b)) * (pitcher_p2b / (1 - pitcher_p2b)) / (league_p2b / (1 - league_p2b)))
-# odds3b = ((batter_p3b / (1 - batter_p3b)) * (pitcher_p3b / (1 - pitcher_p3b)) / (league_p3b / (1 - league_p3b)))
-# oddshr = ((batter_phr / (1 - batter_phr)) * (pitcher_phr / (1 - pitcher_phr)) / (league_phr / (1 - league_phr)))
-# oddsbb = ((batter_pbb / (1 - batter_pbb)) * (pitcher_pbb / (1 - pitcher_pbb)) / (league_pbb / (1 - league_pbb)))
-# oddsso = ((batter_pso / (1 - batter_pso)) * (pitcher_pso / (1 - pitcher_pso)) / (league_pso / (1 - league_pso)))
-# oddsbo = ((batter_pbo / (1 - batter_pbo)) * (pitcher_pbo / (1 - pitcher_pbo)) / (league_pbo / (1 - league_pbo)))
-# 
-# #
-# #
-# # turn odds from odds calculator  into probabilities
-# 
-# p1b = odds1b / (odds1b +1)
-# p2b = odds2b / (odds2b +1)
-# p3b = odds3b / (odds3b +1)
-# phr = oddshr / (oddshr +1)
-# pbb = oddsbb / (oddsbb +1)
-# pso = oddsso / (oddsso +1)
-# pbo = oddsbo / (oddsbo +1)
-# total = p1b + p2b + p3b + phr + pbb + pso + pbo
-# 
-# #
-# #
-# # normalize probabilities to equal 1
-# 
-# np1b = p1b / total
-# np2b = p2b / total
-# np3b = p3b / total
-# nphr = phr / total
-# npbb = pbb / total
-# npso = pso / total
-# npbo = pbo / total
-# =============================================================================
+    p1b = odds1b / (odds1b +1)
+    p2b = odds2b / (odds2b +1)
+    p3b = odds3b / (odds3b +1)
+    phr = oddshr / (oddshr +1)
+    pbb = oddsbb / (oddsbb +1)
+    pso = oddsso / (oddsso +1)
+    pbo = oddsbo / (oddsbo +1)
+    total = p1b + p2b + p3b + phr + pbb + pso + pbo
+    
+    np1b = p1b / total
+    np2b = p2b / total
+    np3b = p3b / total
+    nphr = phr / total
+    npbb = pbb / total
+    npso = pso / total
+    npbo = pbo / total
 
 
 
