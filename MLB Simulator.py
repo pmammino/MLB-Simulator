bff2ffc545490963d4851c50268eb652bddc5cab
pmammino <paul.mammino@wagner.edu>
2018-08-21 13:35:16 -0400

    Update MLB Simulator.py

--- MLB Simulator.py
+++ MLB Simulator.py
@@ -507,14 +507,14 @@ def game_repeater(num_sims, lineup_num):
     away_box_score_total = pandas.DataFrame({'Name' : away_lineup['name'].tolist(),'PA' : [0,0,0,0,0,0,0,0,0],'H': [0,0,0,0,0,0,0,0,0],'BB' : [0,0,0,0,0,0,0,0,0],'Single' : [0,0,0,0,0,0,0,0,0],'Double' : [0,0,0,0,0,0,0,0,0],'Triple' : [0,0,0,0,0,0,0,0,0], 'HR' : [0,0,0,0,0,0,0,0,0], 'R':[0,0,0,0,0,0,0,0,0], 'RBI' : [0,0,0,0,0,0,0,0,0]},columns = ['Name', 'PA', 'H', 'BB', 'Single', 'Double', 'Triple', 'HR', 'R', 'RBI'])
     home_box_score_total = pandas.DataFrame({'Name' : home_lineup['name'].tolist(),'PA' : [0,0,0,0,0,0,0,0,0],'H' : [0,0,0,0,0,0,0,0,0],'BB' : [0,0,0,0,0,0,0,0,0],'Single' : [0,0,0,0,0,0,0,0,0],'Double' : [0,0,0,0,0,0,0,0,0],'Triple' : [0,0,0,0,0,0,0,0,0], 'HR' : [0,0,0,0,0,0,0,0,0], 'R' : [0,0,0,0,0,0,0,0,0], 'RBI' : [0,0,0,0,0,0,0,0,0]},columns = ['Name', 'PA', 'H', 'BB', 'Single', 'Double', 'Triple', 'HR', 'R', 'RBI'])
     home_wins = 0
-  
+    
     for i in range(num_sims):
       away_box, home_box = game_sim(away_lineup, home_lineup, away_pitcher, home_pitcher)
       if home_box['R'].sum() > away_box['R'].sum():
          home_wins = home_wins + 1
       away_box_score_total = away_box_score_total + away_box
       home_box_score_total = home_box_score_total + home_box
-
+    
     del away_box_score_total['Name']
     away_box_score_total = away_box_score_total/num_sims
     away_box_score_total['Name'] = away_lineup['name'].tolist()
