bc794f22da26bf017be31084c3f3f31fc223a334
amock419 <amock419@gmail.com>
2019-06-19 15:47:29 -0400

    Update MLB Game Sim.py

--- MLB Game Sim.py
+++ MLB Game Sim.py
@@ -3,8 +3,8 @@ import time
 import random
 
 lineup = pd.read_csv("MLB Lineup Sim Tool.csv")
-away_team = "HOU"
-home_team = "CIN"
+away_team = "DET"
+home_team = "PIT"
 away_lineup = {1: {'id': lineup.iloc[0, 0], 'x1b': lineup.iloc[0, 1], 'x2b': lineup.iloc[0, 2], 'x3b': lineup.iloc[0, 3], 'xhr': lineup.iloc[0, 4], 'xk': lineup.iloc[0, 5], 'xbb': lineup.iloc[0, 6], 'xout': lineup.iloc[0, 7]},
                2: {'id': lineup.iloc[1, 0], 'x1b': lineup.iloc[1, 1], 'x2b': lineup.iloc[1, 2], 'x3b': lineup.iloc[1, 3], 'xhr': lineup.iloc[1, 4], 'xk': lineup.iloc[1, 5], 'xbb': lineup.iloc[1, 6], 'xout': lineup.iloc[1, 7]},
                3: {'id': lineup.iloc[2, 0], 'x1b': lineup.iloc[2, 1], 'x2b': lineup.iloc[2, 2], 'x3b': lineup.iloc[2, 3], 'xhr': lineup.iloc[2, 4], 'xk': lineup.iloc[2, 5], 'xbb': lineup.iloc[2, 6], 'xout': lineup.iloc[2, 7]},
@@ -67,14 +67,21 @@ third = 0
 home = 0
 
 away_runs = 0
+away_runs_previous = 0
 runs = 0
+runs_previous = 0
 home_runs = 0
 away_order = 1
 home_order = 1
 
+away_wins = 0
+home_wins = 0
+ties = 0
 
-innings = 9
-num_sims = 50000
+
+
+innings = 5
+num_sims = 100000
 x = random.randint(0,100000)
 y = (innings * 3) * num_sims
 
@@ -95,6 +102,9 @@ while away_outs < y or home_outs < y:
                 third = 0
             if away_outs % (innings * 3) == 0:
                 away_order = 1
+                
+                away_runs_game = away_runs - away_runs_previous
+                away_runs_previous = away_runs
        
         elif x > away_lineup[away_order]['xout'] and x < away_lineup[away_order]['xbb']:
             if first == 0 and second == 0 and third == 0:
@@ -244,6 +254,9 @@ while away_outs < y or home_outs < y:
                 third = 0
             if home_outs % (innings * 3) == 0:
                 home_order = 1
+                
+                runs_game = runs - runs_previous
+                runs_previous = runs
        
         elif x > home_lineup[home_order]['xout'] and x < home_lineup[home_order]['xbb']:
             if first == 0 and second == 0 and third == 0:
@@ -382,6 +395,14 @@ while away_outs < y or home_outs < y:
         home_order = home_order + 1
         if home_order == 10:
             home_order = 1
+        
+        if away_outs % (innings * 3) == 0 and home_outs % (innings * 3) == 0:
+            if runs_game > away_runs_game:
+                home_wins = home_wins + 1
+            if runs_game < away_runs_game:
+                away_wins = away_wins + 1
+            else:
+                ties = ties + 1
             
 
 hfa = .153
@@ -390,6 +411,8 @@ home_rpg = round((home_runs / num_sims) + (hfa / 2), 4)
 total_rpg = round((away_runs + home_runs) / num_sims, 4)
 away_win = round(away_rpg ** 1.83 / ((away_rpg ** 1.83) + (home_rpg ** 1.83)), 4)
 home_win = round(1 - away_win, 4)
+away_win_count = away_wins
+home_win_count = home_wins
 
 if away_win > .5:
     away_ml = round(away_win / (1 - away_win) * -100, 0)
@@ -400,7 +423,14 @@ if home_win > .5:
     home_ml = round(home_win / (1 - home_win) * -100, 0)
 else:
     home_ml = round((1 - home_win) / home_win * 100, 0)
-         
+
+if innings == 9:
+    print("Full game results...")
+if innings == 5:
+    print("First five results...")
+else:
+    print("First inning results...")
+        
 if away_outs or home_outs == y:
     print(str(away_team) + ' - ' + str(away_win) + ' % / ' + str(away_ml) + ' / ' + str(away_rpg) + ' runs')
     
@@ -413,6 +443,5 @@ clock = (time.time() - start_time)
 if clock >= 60:
     print("The simulation took " + str(round(clock / 60, 4)) + " minutes to complete.")
 else:
-    print("The simulation took " + str(round(clock, 4)) + " seconds to complete.")
-
+    print("The " + str(num_sims) + " simulations took " + str(round(clock, 4)) + " seconds to complete.")
 
